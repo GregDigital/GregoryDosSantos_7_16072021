11f20aedf2678b1166998d4ca4db0d830dca5208
GregDigital <gdossantos.pro@gmail.com>
2021-08-16 19:41:40 +0200

    function sortREcipes

--- javascript/script.js
+++ javascript/script.js
@@ -9,11 +9,50 @@ async function fetchRecipes() {
     .then((data) => {
       recipesJson = data.recipes;
       displayRecipes(recipesJson);
-      inputSearch();
+      sortRecipes();
     });
 }
 
 // ================= fonction qui permet de générer le contenu des recettes=============
+
+// ===================== Input principal de recherche ===================
+const inputSearch = document.querySelector('input[type="search"]');
+
+function sortRecipes() {
+  inputSearch.addEventListener("input", (e) => {
+    let valueInput = e.target.value;
+    const dataId = document.querySelectorAll("[data-id]");
+    console.log(dataId);
+    if (valueInput.length >= 3) {
+      dataId.forEach((element) => {
+        element.style.visibility = "hidden";
+      });
+      //
+      let value = Object.keys(recipesJson);
+      for (let i = 0; i < value.length; i++) {
+        let description = recipesJson[value[i]].description;
+        let verifOccurrences = description.includes(valueInput); // test occurences des mots dans la description des recettes et l'input
+        let matched_recipes = []; // tableau vide pour recuperer les recettes ayant le mot correspondant
+        let index = 0;
+        let id = recipesJson[value[i]].id;
+        if (verifOccurrences == true) {
+          let arrayId = id++;
+          console.log(arrayId);
+
+          matched_recipes.push(arrayId);
+          console.log(matched_recipes);
+        }
+
+        //displayRecipes(matched_recipes);
+      }
+    } else {
+      dataId.forEach((element) => {
+        element.style.visibility = "visible";
+      });
+    }
+  });
+}
+
 function displayRecipes() {
   let containerRecipes = [];
   for (let recipe of recipesJson) {
@@ -22,33 +61,38 @@ function displayRecipes() {
   let html = containerRecipes.reduce((a, l) => a + l);
   container.innerHTML = html;
 }
-// ===================== Input principal de recherche ===================
 
-function inputSearch() {
+/*function inputSearch() {
+  // la variable devrait être accessible gloabalement, tu vas vouloir la réutiliser plusieurs fois
+  // aussi attention au nommage, ici la variable a le mm nom que la fonction, c'est un peu confusant
   const inputSearch = document.querySelector('input[type="search"]');
 
   inputSearch.addEventListener("input", (e) => {
-    const dataId = document.querySelectorAll("[data-id]");
+    const dataId = document.querySelectorAll("[data-id]"); // on parle bien des 4 champs de recherche ?
 
     let valueInput = e.target.value;
 
     if (valueInput.length >= 3) {
+      // utilise la fonction forEach() pour itérer sur les recettes, ce sera plus court à écrire et l'intention plus explicite
       let value = Object.keys(recipesJson);
 
       for (let i = 0; i < value.length; i++) {
         let description = recipesJson[value[i]].description;
         let verifOccurrences = description.includes(valueInput); // test occurences des mots dans la description des recettes et l'input
         let matched_recipes = []; // tableau vide pour recuperer les recettes ayant le mot correspondant
-        let index = 0;
+        // attention l'index ne correspond pas à l'id d'une recette, il ne te permettra pas de retrouver une recette
+        let index = 0; data-id
         if (verifOccurrences == true) {
           index++;
+          // au lieu de push() l'index push directement la recette : matched_recipes.push(value[i])
           matched_recipes.push(index);
           console.log(matched_recipes);
         }
+        // il reste à afficher les recettes : displayRecipes(matched_recipes)
       }
     }
   });
-}
+}*/
 
 // ==================== fonction qui permet de générer les ingredients dans le li =======
 
